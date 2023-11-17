9f3e424d8e1300279943ad523c4075f49c7d844e
laloyola <llloyola@uc.cl>
2023-11-17 16:39:50 -0300

    add categories to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,48 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledWelcome = styled.div`
+  height: 800px;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  padding: 10px;
+`;
+
+const StyledOptions = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+`;
+
+const StyledOption = styled.div`
+  display: flex;
+  min-height: 100px;
+  min-width: 100px;
+  flex: 1 0 0;
+  align-items: center;
+  justify-content: center;
+  padding: 10px;
+  border: 1px solid ${({ theme }) => theme.primaryColor};
+  border-radius: 5px;
+  margin: 5px;
+  cursor: pointer;
+`;
 
 const Home = () => (
   <main>
-    <h1>Home Page</h1>
+    <StyledWelcome>
+      <h1>Welcome to the Home Page</h1>
+      <StyledOptions>
+        <StyledOption>Movies</StyledOption>
+        <StyledOption>TV Shows</StyledOption>
+        <StyledOption>Actors and actresses</StyledOption>
+      </StyledOptions>
+    </StyledWelcome>
   </main>
 );
 
