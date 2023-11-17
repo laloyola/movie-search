5ac05b5c9d128e6ed30e3aa5f3a073801084a216
laloyola <llloyola@uc.cl>
2023-11-17 15:14:00 -0300

    add initial Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">Learn React</header>
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      {/* Add routes later */}
+    </Routes>
+  </Router>
+);
 
 export default App;
