69107a1d951987bcc91c5449e5d0e64c6da37192
laloyola <llloyola@uc.cl>
2023-11-18 16:22:11 -0300

    add movies route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Movies from './pages/Movies';
 import Navbar from './components/common/Navbar';
 import Footer from './components/common/Footer';
 
@@ -9,6 +10,7 @@ const App = () => (
     <Navbar />
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/movies" element={<Movies />} />
       {/* Add routes later */}
     </Routes>
     <Footer />
