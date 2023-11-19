29f675aa7b3ef3bb0fac18b600cefe9edeb87c94
laloyola <llloyola@uc.cl>
2023-11-19 01:00:28 -0300

    add layout to movies page

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,5 +1,52 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Movies = () => <h1>Movies</h1>;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ListsContainer = styled.div`
+  width: 90%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
+`;
+
+const List = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 10px;
+  border: 1px solid ${({ theme }) => theme.primaryColor};
+  border-radius: 5px;
+  width: 300px;
+`;
+
+const Movies = () => (
+  <Container>
+    <h1>Movies</h1>
+    <ListsContainer>
+      <List>
+        <h2>Now Playing</h2>
+        <p>Movies playing now</p>
+      </List>
+      <List>
+        <h2>Popular</h2>
+        <p>Popular movies</p>
+      </List>
+      <List>
+        <h2>Top Rated</h2>
+        <p>Top rated movies</p>
+      </List>
+      <List>
+        <h2>Upcoming</h2>
+        <p>Upcoming movies</p>
+      </List>
+    </ListsContainer>
+  </Container>
+);
 
 export default Movies;
